aef22c3db5b210be7cc3dbb4a42e9c627a84a8a4
vaithiyanathan <vaithiyanathan30@gmail.com>
2024-03-29 15:26:33 +0530

    test

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="bg-black text-white py-4 text-center">
-      <h1 className="text-2xl font-semibold">GIS BASED DATA VISUALIZATION FOR CROP ANALYSIS</h1>
+      <h1 className="text-2xl font-semibold">GIS BASED DATA VISUALIZATION FOR CROP ANALYSIS (terra tech hackathon)</h1>
     </header>
   );
 };
